c6df98c9b04e894355fe89df09fe10be604123b8
likesatie@gmail.com <likesatie@gmail.com>
2018-10-13 21:50:32 +0300

    Refactored some repetitive code in script

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// play again event listener
+game.addEventListener('click', function(e) {
+  if (e.target.className === 'play-again') {
+    window.location.reload();
+  }
+});
+
 // listen for guess
 guessBtn.addEventListener('click', function() {
   let guess = parseInt(guessInput.value);
@@ -29,12 +36,50 @@ guessBtn.addEventListener('click', function() {
 
   // check if won
   if (guess === winningNum) {
-    guessInput.disabled = true;
-    guessInput.style.borderColor = 'green';
-    setMessage(`${winningNum} Верно! Победа!`, 'green');
+    // guessInput.disabled = true;
+    // guessInput.style.borderColor = 'green';
+    // setMessage(`${winningNum} Верно! Победа!`, 'green');
+    gameOver(true, setMessage(`${winningNum} Верно! Победа!`, 'green'));
   } else {
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // guessInput.disabled = true;
+      // guessInput.style.borderColor = 'red';
+      // setMessage(
+      //   `Игра окончена, вы проиграли. Верный ответ - ${winningNum}`,
+      //   'red'
+      // );
+      gameOver(
+        true,
+        setMessage(
+          `Игра окончена, вы проиграли. Верный ответ - ${winningNum}`,
+          'red'
+        )
+      );
+    } else {
+      guessInput.style.borderColor = 'red';
+      guessInput.value = '';
+      setMessage(
+        `${guess} Попытка не верная, попыток осталось: ${guessesLeft}`,
+        'red'
+      );
+    }
   }
 });
+
+// game over
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = 'green') : (color = 'red');
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg);
+
+  // play again?
+  guessBtn.value = 'Играть снова';
+  guessBtn.className += 'play-again';
+}
 
 // set message
 function setMessage(msg, color) {
